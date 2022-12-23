79c4c50dfc30b88b6861ddb59db9388bb63b1975
Bismark Arroyo <bis.arroyo@gmail.com>
2022-12-22 22:53:35 -0600

    added text to radar effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,45 @@ import { motion } from 'framer-motion'
 
 import './App.scss'
 
+function Text() {
+  return (
+    <div className='text-radar'>
+      <p className='text-top-left'><span>The time<br />is</span> Running Out<span>.</span></p>
+      <p className='text-bottom-left'><span>Get<br /></span> Motivated<span>.</span></p>
+      <p className='text-bottom-right'><span>Don´t stop<br /></span>Working<span> hard.</span></p>
+      <p className='text-top-right'><span>Focus on<br /> your</span> Goal<span>.</span></p>
+    </div>
+  )
+}
+
+
+
+
 function App() {
 
   return (
-    <motion.div
-      initial={{opacity:0}}
-      animate={{opacity:1}}
-      transition={{duration:3}}
-      className="radar"
-    >
+    <>
+      <Text />
       <motion.div
-        animate={{
-          rotate: -360
-        }}
-        transition={{
-          ease: "linear",
-          duration: 7,
-          repeat: Infinity
-        }}
-        className="pointer"
-      ></motion.div>
-    </motion.div>
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{duration:3}}
+        className="radar"
+      >
+        <motion.div
+          animate={{
+            rotate: -360
+          }}
+          transition={{
+            ease: "linear",
+            duration: 7,
+            repeat: Infinity
+          }}
+          className="pointer"
+        >
+        </motion.div>
+      </motion.div>
+    </>
   )
 }
 
